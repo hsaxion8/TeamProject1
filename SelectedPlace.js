9593c4aeac8053d099d71a901fbfb99743b28835
Lexie_Murphy <Lexie.Murphy88@gmail.com>
2019-01-19 01:17:26 -0600

    got api set up and pulling in data for restaurants since coupon idea was a bust.

--- SelectedPlace.js
+++ SelectedPlace.js
@@ -1,40 +1,33 @@
 $(document).ready(function () {
 
-    $(document).on("click", ".select-btn", displayPlaceCoupons);
+    $(document).on("click", ".select-btn", displayPlaceRestaurantDetails);
 
 
 
     // displayPlaceCoupons function re-renders the HTML to display the appropriate content
-    function displayPlaceCoupons() {
+    function displayPlaceRestaurantDetails() {
 
 
         // variable for selected Place
-        var place = "Walmart";
+        var place = "16774318";
         console.log(place);
 
-        var place = $(this).attr("selected-place");
+        //var place = $(this).attr("selected-place");
         // Constructing a queryURL using the topic
 
 
-        // These code snippets use an open-source library. http://unirest.io/nodejs
-        var queryURL = "https://udayogra-deal-and-coupons-v1.p.rapidapi.com/dl?merchant=mcdonalds&submittype=deals&action=getoffersbymerchant" + "&api_key=b8a28035afmshd00b7fe0009132bp116bdbjsn15bf0056f371&limit=10";
+        // Pull in data with zomato api
+        var queryURL = "https://developers.zomato.com/api/v2.1/restaurant?res_id="+ place +"&api_key=40dfa72638fdd4bc99a0bb21a8c4d08f&limit=10";
         console.log(queryURL);
 
         $.ajax({
             method: "GET",
             url: queryURL,
             headers: {
-                "X-RapidAPI-Key": "b8a28035afmshd00b7fe0009132bp116bdbjsn15bf0056f371"
+                "Accept": "application/json",
+                "user-key": "40dfa72638fdd4bc99a0bb21a8c4d08f",
             }
         })
-
-
-
-
-            //var queryURL = "https://udayogra-deal-and-coupons-v1.p.mashape.com/dl?action=getoffersbymerchant&merchant=" +
-            //place + "&submittype=deals&api_key=J78jBfpm8omshpD4Gx4GUB0YbkWBp1fsQkYjsn2PE7Q9e1P7cb&limit=10";
-
-
 
             // After data comes back from the request store the data results in the variable
             .then(function (response) {
@@ -42,23 +35,30 @@ $(document).ready(function () {
 
                 var results = response.data;
 
-                $("#coupons-view").empty();
+                $("#restaurant-view").empty();
                 // Loop through each result item and create div for rating and image
-                for (var i = 0; i < results.length; i++) {
+                for (var i = 0; i < results; i++) {
 
-                    var couponDiv = $("<div>");
-                    var couponImage = $("<img>");
-                    couponImage.addClass("placeCoupons");
-                    couponImage.attr("src", results[i].images.fixed_height_still.url);
-                    var p = $("<p>").text("Rating: " + results[i].rating);
+                    var restaurantDetailsDiv = $("<div>");
+                    var restaurantDetailsImage = $("<img>");
+                    restaurantDetailsImage.addClass("placeRestaurantDetails");
+                    restaurantDetailsImage.attr("featured_image", results[i].featured_image);
+                    var p = $("<p>").text("Cuisines: " + results[i].cuisines);
+                    var p = $("<p>").text("Average Cost for Two: $");
+                    var p = $("<p>").text("Link to Menu:" + resutls[i].menu_url);
+                    var p = $("<p>").text("Restaurant Rating:" + results[i].user_rating.aggregate_rating.rating_test);
+
+                    console.log(restaurantDetailsDiv);
+                    console.log(restaurantDetailsImage);
+                    console.log(p);
 
                     // Appending the paragraph and image tag to the topicDiv
-                    couponDiv.append(couponImage);
-                    couponDiv.append(p);
+                    restaurantDetailsDiv.append(restaurantDetailsImage);
+                    restaurantDetailsDiv.append(p);
 
 
                     // Prependng the topicDiv to the HTML page in the gifs div
-                    $("#coupons-view").prepend(couponDiv);
+                    $("#coupons-view").prepend(restaurantDetailsDiv);
                 }
             });
 
