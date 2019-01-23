82da9e975bb9bc093ecef2bf3f6e8fe50670b7f2
Jaybird <jrmiller022@gmail.com>
2019-01-22 23:26:19 -0600

    updated corrects to new map and geocode

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,26 @@
-var map;
-   var infowindow;
-   function initMap() {
-       var pyrmont = {lat: 38.898945, lng: -94.725541};
-
-       map = new google.maps.Map(document.getElementById('map'), {
-           center: pyrmont,
-           zoom: 13
-       });
-
-       infowindow = new google.maps.InfoWindow();
-       var service = new google.maps.places.PlacesService(map);
-       service.nearbySearch({
-           location: pyrmont,
-           radius: 4000,
-           type: ['restaurant']
-       }, callback);
-   }
-
-   function callback(results, status) {
-       if (status === google.maps.places.PlacesServiceStatus.OK) {
-           for (var i = 0; i < results.length; i++) {
-               createMarker(results[i]);
-           }
-       }
-   }
-
-   function createMarker(place) {
-       var placeLoc = place.geometry.location;
-       var marker = new google.maps.Marker({
-           map: map,
-           position: place.geometry.location
-       });
-
-       google.maps.event.addListener(marker, 'click', function () {
-           infowindow.setContent(place.name);
-           infowindow.open(map, this);
-       });
-   }
-;
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 8,
+      center: {lat: -34.397, lng: 150.644}
+    });
+    var geocoder = new google.maps.Geocoder();
+ 
+    document.getElementById('submit').addEventListener('click', function() {
+      geocodeAddress(geocoder, map);
+    });
+  }
+ 
+  function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === 'OK') {
+        resultsMap.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
